cd5a1bd05634a58823178f6047f2b0068d6955da
Talha Zahid <talha.zahid86@gmail.com>
2022-07-07 15:52:57 +0500

    lexjs testing

--- js/custom.js
+++ js/custom.js
@@ -331,3 +331,31 @@ $(document).scroll((e) => {
 })();
 
 
+
+
+
+
+
+
+window.onload = function () {
+ lax.init()
+
+ // Add a driver that we use to control our animations
+ lax.addDriver('scrollY', function () {
+  return window.scrollY
+ })
+
+ // Add animation bindings to elements
+ lax.addElements('.left-pan', {
+  scrollY: {
+   translateX: [
+    ["elInY", "elCenterY", "elOutY"],
+    [0, 'screenWidth/2', 'screenWidth'],
+   ]
+  }
+ })
+}
+
+
+
+
